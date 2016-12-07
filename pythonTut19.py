34463c17b51b152f6dba54294dd1c3f838c8b400
Michael <mdimovich@gmail.com>
2016-12-06 17:01:57 -0800

    Added prime number code

--- pythonTut19.py
+++ pythonTut19.py
@@ -1,3 +1,6 @@
+# Prime number generator
+# Check and see which numbers in a specified range are prime
+# User inputs max number to check, and every prime is found up to that number
 def isPrime(num):
         for i in range (2, num):
             if (num % i == 0):
@@ -12,5 +15,12 @@ def getPrimes(max_number):
         if isPrime(num1):
             listOfPrimes.append(num1)
 
+    return listOfPrimes
+
+max_num_to_check = int(input("Search for primers up to: "))
+list_of_primes = getPrimes(max_num_to_check)
+
+for prime in list_of_primes:
+    print(prime)
 
 
